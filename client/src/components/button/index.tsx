227e653555ad663c17fc0292e9a8dce3fd2d9cb3
xarcgit <yasouo@proton.me>
2024-11-04 23:46:47 +0530

    chore: remove unused files and configurations; set up basic Vue app structure

--- client/src/components/button/index.tsx
+++
@@ -1,5 +0,0 @@
-import { component$ } from '@builder.io/qwik';
-
-export default component$(() => {
-	return <button type="button">Click me</button>;
-});
--- server/validations/request.validation.ts
+++ server/validations/request.validation.ts
@@ -1,6 +1,5 @@
 import { z } from 'zod';
 
-<<<<<<< HEAD
 export const getRequestSchema = z.object({
 	filterTags: z
 		.array(z.string())
@@ -9,15 +8,4 @@ export const getRequestSchema = z.object({
 
 export const refreshRequestSchema = z.object({
 	forceRefresh: z.boolean(),
-||||||| 88693b6
-export const requestSchema = z.object({
-	tags: z.array(z.string()),
-=======
-export const getRequestSchema = z.object({
-	filterTags: z.array(z.string()),
-});
-
-export const refreshRequestSchema = z.object({
-	forceRefresh: z.boolean(),
->>>>>>> 54b1bca46d85f331342f3372c6d2034348b3fe8c
 });
