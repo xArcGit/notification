fb9b89616d6eeca2d091c7fb70c11846bf1c26a7
xarcgit <yasouo@proton.me>
2024-12-02 13:42:15 +0530

    add

--- server/app.ts
+++ server/app.ts
@@ -1,30 +1,30 @@
-import { httpStatus, httpStatusMessages } from '@utils/http-status';
+import { httpStatus, httpStatusMessages } from "@utils/http-status";
 
-import ApiError from './utils/api-error';
-import { Hono } from 'hono';
-import { cors } from 'hono/cors';
-import { logger as customLogger } from '@utils/logger';
-import { errorHandler } from './middlewares/error-handler';
-import { logger } from 'hono/logger';
-import { route } from './routes';
-import { sentry } from '@hono/sentry';
+import ApiError from "./utils/api-error";
+import { Hono } from "hono";
+import { cors } from "hono/cors";
+import { logger as customLogger } from "@utils/logger";
+import { errorHandler } from "./middlewares/error-handler";
+import { logger } from "hono/logger";
+import { route } from "./routes";
+import { sentry } from "@hono/sentry";
 
 const app = new Hono();
-app.use('*', sentry());
-app.use('*', cors());
+app.use("*", sentry());
+app.use("*", cors());
 
 app.use(
-	'*',
-	logger((message, ...rest) => customLogger.info(message, ...rest)),
+  "*",
+  logger((message, ...rest) => customLogger.info(message, ...rest)),
 );
 
-app.route('/notifications', route);
+app.route("/notifications", route);
 
 app.notFound(() => {
-	throw new ApiError(
-		httpStatus.NOT_FOUND,
-		httpStatusMessages[httpStatus.NOT_FOUND],
-	);
+  throw new ApiError(
+    httpStatus.NOT_FOUND,
+    httpStatusMessages[httpStatus.NOT_FOUND],
+  );
 });
 
 app.onError(errorHandler);
